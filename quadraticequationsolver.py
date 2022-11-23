1d90d032f806f42979e8344f966abacfa2571c43
CurseD-PlayeR <115417800+CurseD-PlayeR@users.noreply.github.com>
2022-11-23 22:43:04 +0300

    Update quadraticequationsolver.py

--- quadraticequationsolver.py
+++ quadraticequationsolver.py
@@ -1,19 +1,24 @@
 from math import sqrt
+x1 = 0
+x2 = 0
 print('Enter three coefficients for this formula: ')
 print('a*x^2 + b*x + c = 0')
 a = float(input('a = '))
-b = float(input('b = '))
-c = float(input('c = '))
-D = b**2 - 4*a*c
-x1 = 0
-x2 = 0
-if D>0:
-    x1 = (-b + sqrt (D))/2
-    x2 = (-b - sqrt (D))/2
-    print ('First root: ', x1)
-    print ('Second root: ', x2)
-if D==0:
-    x1 = (-b)/2
-    print('The only root: ', x1)
-if D<0:
-    print('No rational roots exist.')
+if a !=0:
+    b = float(input('b = '))
+    c = float(input('c = '))
+    D = b**2 - 4*a*c
+    if D>0:
+        x1 = (-b + sqrt (D))/2
+        x2 = (-b - sqrt (D))/2
+        print ('First root: ', x1)
+        print ('Second root: ', x2)
+    if D==0:
+        x1 = (-b)/2
+        print('The only root: ', x1)
+    if D<0:
+        print('No rational roots exist.')
+else:
+    print ('For a quadratic equation coefficient "a" must not be equal to 0')
+    print ('Restart and enter a different coefficient for "a"')
+        
